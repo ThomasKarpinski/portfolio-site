4bf35b046af1fad3918b4a1e31fbf9a0c3a38d4d
ThomasKarpinski <tomaszkarpinski204@gmail.com>
2022-08-24 20:19:59 +0200

    slider

--- static/vanila.js
+++ static/vanila.js
@@ -1,9 +1,16 @@
-import VanillaTilt from "vanilla-tilt";
 VanillaTilt.init(document.querySelector(".background"), {
     glare: true,
     reverse: true,
     "max-glare": 0.5,
     startX: 45,
     startY: 45,
+    reset: false
+});
+VanillaTilt.init(document.querySelector(".slider"), {
+    glare: true,
+    reverse: true,
+    "max-glare": 0.5,
+    startX: -45,
+    startY: -45,
     reset: false
 });
