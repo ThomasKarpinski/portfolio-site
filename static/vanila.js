d2adad08f0308ef19f53ef4723efd3eaaea20de8
ThomasKarpinski <tomaszkarpinski204@gmail.com>
2022-08-26 15:55:40 +0200

    slogan

--- static/vanila.js
+++ static/vanila.js
@@ -8,6 +8,13 @@ VanillaTilt.init(document.querySelector(".background"), {
 });
 VanillaTilt.init(document.querySelector(".slider"), {
     glare: true,
+    "max-glare": 0.5,
+    startX: -45,
+    startY: -45,
+    reset: false
+});
+VanillaTilt.init(document.querySelector(".slogan"), {
+    glare: true,
     reverse: true,
     "max-glare": 0.5,
     startX: -45,
