5844c63803028dbca9f971244d0bf9358d12456e
ThomasKarpinski <tomaszkarpinski204@gmail.com>
2022-08-29 01:13:38 +0200

    .

--- static/navbar.js
+++ static/navbar.js
@@ -1,3 +1,23 @@
 const link = () =>{
     location.href = "/home";
 }
+const nav = document.querySelector('nav');
+const buttons = document.querySelector('.navbar_buttons');
+const logo = document.querySelector('.logo');
+const value = 200
+
+const changeNav = () => {
+    nav.classList.toggle('shrink', window.scrollY > value)
+}
+
+const changeButtons = () => {
+    buttons.classList.toggle('navbar_buttons2', window.scrollY > value)
+}
+
+const hideLogo = () => {
+    logo.classList.toggle('logo2', window.scrollY > value)
+}
+
+window.addEventListener('scroll', changeNav)
+window.addEventListener('scroll', changeButtons)
+window.addEventListener('scroll', hideLogo)
