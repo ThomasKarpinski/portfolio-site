fb8b544f4dca2e51a940df6b40c296a5b0665af3
ThomasKarpinski <tomaszkarpinski204@gmail.com>
2023-03-23 19:35:54 +0100

    form

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request
-import mysql.connector
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,26 +7,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("templates.html")
-
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    text = request.form['text']
-
-    db = mysql.connector.connect(
-        host="localhost",
-        user="username",
-        password="password",
-        database="database_name"
-    )
-
-    cursor = db.cursor()
-    cursor.execute("INSERT INTO table_name (text) VALUES (%s)", (text,))
-    db.commit()
-
-    db.close()
-
-    return "Text added to database successfully!"
 
 
 if __name__ == "__main__":
